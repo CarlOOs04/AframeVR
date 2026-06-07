cd8e2ca8a52b2c92e2d581f6602b000d0b1e323b
CarlOOs04 <241709260+CarlOOs04@users.noreply.github.com>
2026-06-07 21:33:04 +0200

    Delete Informatica_VR/js directory

--- Informatica_VR/js/sensors-sim.js
+++
@@ -1,7 +0,0 @@
-setInterval(() => {
-    let temp = Math.floor(Math.random() * 40 + 30);
-    const display = document.getElementById('temp-display');
-    if (display) {
-        display.setAttribute('value', 'Temp. CPU: ' + temp + '°C');
-    }
-}, 2000);
